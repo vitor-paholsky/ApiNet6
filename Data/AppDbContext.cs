290382c60d138d7e7972896a4233d1a0d7645d0f
Vitor Paholsky <vitor.duarte@fulltechpro.com.br>
2022-01-21 15:46:22 -0300

    Adicionar arquivos de projeto.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ApiNet6.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiNet6.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TodoModel> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("conectionstring");
+    }
+}
